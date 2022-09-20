562e34ac4902b5a29a701824c8ba3003d48e1e75
Milad Hassani <suchendrahassani@gmail.com>
2022-09-20 05:59:00 -0400

    changed to fetch instead of request

---
+++ backend/routes/auth_spotify.js
@@ -0,0 +1,63 @@
+/**
+ * This is an example of a basic node.js script that performs
+ * the Client Credentials oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ */
+
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+const fetch = require("node-fetch");
+
+const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
+
+var urlencoded = new URLSearchParams();
+urlencoded.append("grant_type", "client_credentials");
+
+var requestOptions = {
+    method: "POST",
+    headers: {
+        Authorization: "Basic " + Buffer.from(client_id + ":" + client_secret).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: urlencoded,
+    redirect: "follow",
+};
+
+async function run() {
+    // your application requests authorization
+    let token;
+    fetch("https://accounts.spotify.com/api/token", requestOptions)
+        .then((res) => res.json())
+        .then((res) => (token = res));
+
+    // let res = await request.post(authOptions, function (error, response, body) {
+    //     // console.log(body);
+    //     if (!error && response.statusCode === 200) {
+    //         // use the access token to access the Spotify Web API
+    //         token = body.access_token;
+    //         // var options = {
+    //         //     url: "https://api.spotify.com/v1/users/jmperezperez",
+    //         //     headers: {
+    //         //         Authorization: "Bearer " + token,
+    //         //     },
+    //         //     json: true,
+    //         // };
+    //         // request.get(options, function (_error, _response, body) {
+    //         //     console.log(body);
+    //         // });
+    //     }
+    // });
+    // console.log(res);
+}
+
+router.get("", run);
+
+module.exports = {
+    path: "/auth", // The main entrance to this route aka /
+    handler: router, // The router object that handles this route
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,5 @@
-// const blazemeter = require("./blazemeter");
+const auth = require("./auth_spotify.js");
 
-// module.exports.routes = [
-//   blazemeter, //
-//   appdynamics, // Handles connection with AppDynamics api, processes results and sends to client
-//   // query, // Connect to Oracle DB and run pre-defined queries, send results
-//   metrics, // Endpoint to return the metrics between start-end time.
-//   timescale, // Endpoint to return the oracle query results (performed by query agent) from timescale DB
-//   validate, // for test running custom query / db settings
-// ];
+module.exports.routes = [
+    auth, // authentication process for spotify
+];
